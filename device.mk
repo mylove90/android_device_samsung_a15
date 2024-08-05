0f3ff14dc8d6485fe4ba2b6557ddd3e70c91e3cc
YZBruh <yagizzengin73@gmail.com>
2024-08-05 06:42:44 +0000

    a155f: Make general corrections and edits

--- device.mk
+++ device.mk
@@ -1,10 +1,5 @@
 LOCAL_PATH := device/samsung/a15
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 31
-
-# API
-PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # fastbootd
 PRODUCT_PACKAGES += \
@@ -14,7 +9,24 @@ PRODUCT_PACKAGES += \
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
+
+# EROFS Tools
+PRODUCT_HOST_PACKAGES_ENG += \
+    liberofs \
+    mkfs.erofs \
+    make_erofs \
+    dump.erofs \
+    fsck.erofs
+
+PRODUCT_PACKAGES += shrink
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
--- twrp_a15.mk
+++ twrp_a15.mk
@@ -1,6 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -15,11 +15,6 @@ PRODUCT_MODEL := SM-A155F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a15nsxx-user 12 SP1A.210812.016 A155FXXS3BXF1 release-keys"
-
-BUILD_FINGERPRINT := samsung/a15nsxx/a15:12/SP1A.210812.016/A155FXXS3BXF1:user/release-keys
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
